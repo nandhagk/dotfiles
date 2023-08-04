5b8d8509821be339411930206e55c5d9a148e722
nandhagk <62976649+nandhagk@users.noreply.github.com>
2023-08-04 10:17:04 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,6 @@ set -gx fish_greeting ''
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# pnpm
-set -gx PNPM_HOME "/home/nandhagk/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
